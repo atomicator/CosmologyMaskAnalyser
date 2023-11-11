fd7caf1d5c4f853925e9c9147c9213a5bdc8ba30
Tom <wardt4996@gmail.com>
2023-11-11 22:04:38 +0000

    test

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ import setuptools
 setuptools.setup(
     name="toolkit",
     version="1.0",
+    packages=["toolkit"],
     install_requires=["numpy", "scipy", "healpy", "pixell", "matplotlib", "astropy"],
 )
