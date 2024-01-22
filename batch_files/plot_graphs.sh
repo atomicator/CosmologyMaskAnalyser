15e7225050cece2d9a7abc230b3701d024f36e18
thomas <wardt4996@gmail.com>
2024-01-22 20:36:29 +0000

    changed stuff

--- batch_files/plot_graphs.sh
+++ batch_files/plot_graphs.sh
@@ -7,9 +7,9 @@
 # Write new graphs
 
 python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=1 --save_path="./../graphs/binned_results/sdss_sample_1.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=2 --save_path="./../graphs/binned_results/sdss_sample_2.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=3 --save_path="./../graphs/binned_results/sdss_sample_3.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=2 --save_path="./../graphs/binned_results/sdss_sample_2.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=3 --save_path="./../graphs/binned_results/sdss_sample_3.pdf"
 
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=1 --save_path="./../graphs/binned_results/sdss_random_1.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=2 --save_path="./../graphs/binned_results/sdss_random_2.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=3 --save_path="./../graphs/binned_results/sdss_random_3.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=1 --save_path="./../graphs/binned_results/sdss_random_1.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=2 --save_path="./../graphs/binned_results/sdss_random_2.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=3 --save_path="./../graphs/binned_results/sdss_random_3.pdf"
