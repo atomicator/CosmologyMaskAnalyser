b4fdf9177fbff24c1cb3cacaadc9e6faae52b49b
thomas <wardt4996@gmail.com>
2024-01-22 19:59:42 +0000

    changed stuff

--- batch_files/plot_graphs.sh
+++ batch_files/plot_graphs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Generate random distributions
-python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --nside=2 --target=10 --save_path="./../data/binned_random_catalogues/sdss_nside_2.fits"
-python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --nside=8 --target=10 --save_path="./../data/binned_random_catalogues/sdss_nside_8.fits"
-python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --nside=32 --target=10 --save_path="./../data/binned_random_catalogues/sdss_nside_32.fits"
+python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --nside=2 --target=10 --save_path="./../../data/binned_random_catalogues/sdss_nside_2.fits"
+#python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --nside=8 --target=10 --save_path="./../data/binned_random_catalogues/sdss_nside_8.fits"
+#python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --nside=32 --target=10 --save_path="./../data/binned_random_catalogues/sdss_nside_32.fits"
 
 # Write new graphs
 
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=1 --save_path="./../graphs/binned_results/sdss_1.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=2 --save_path="./../graphs/binned_results/sdss_1.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=3 --save_path="./../graphs/binned_results/sdss_1.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=1 --save_path="./../graphs/binned_results/sdss_1.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=2 --save_path="./../graphs/binned_results/sdss_1.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue=sdss --weight_version=3 --save_path="./../graphs/binned_results/sdss_1.pdf"
 
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=1 --save_path="./../graphs/binned_results/sdss_1.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=2 --save_path="./../graphs/binned_results/sdss_2.pdf"
-python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=3 --save_path="./../graphs/binned_results/sdss_3.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=1 --save_path="./../graphs/binned_results/sdss_1.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=2 --save_path="./../graphs/binned_results/sdss_2.pdf"
+#python3 "./../code/binned_results/results_plot_new.py" --raise_path=1 --catalogue="./../data/binned_random_catalogues/sdss_nside_8.fits" --weight_version=3 --save_path="./../graphs/binned_results/sdss_3.pdf"
