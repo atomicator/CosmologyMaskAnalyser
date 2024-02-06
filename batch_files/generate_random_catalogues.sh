a03d3cbbef8cc9d0b384786315ea2c33480a17fd
thomas <wardt4996@gmail.com>
2024-02-06 17:14:41 +0000

    changed stuff

--- batch_files/generate_random_catalogues.sh
+++ batch_files/generate_random_catalogues.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Generate random distributions
-python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --target=80000 --save_path="./../data/binned_random_catalogues/80k.fits" &
-python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1  --target=400000 --save_path="./../data/binned_random_catalogues/400k.fits" &
-python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1  --target=10000000 --save_path="./../data/binned_random_catalogues/10m.fits" &
+python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1 --target=80000 --save_path="./../code/binned_results/80k.fits" &
+python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1  --target=400000 --save_path="./../code/binned_results/400k.fits" &
+python3 "./../code/binned_results/weighted_random_distribution.py" --raise_path=1  --target=10000000 --save_path="./../code/binned_results/10m.fits" &
 wait
 
 nice -20 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="80k"  \
