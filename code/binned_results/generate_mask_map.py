db6e286fbd9318cb4c4d024255c34a5c3790a38a
thomas <wardt4996@gmail.com>
2024-02-08 15:14:38 +0000

    changed stuff

--- code/binned_results/generate_mask_map.py
+++ code/binned_results/generate_mask_map.py
@@ -8,7 +8,7 @@ parser.add_argument("--mask_one", choices=["sdss_mask", "planck_point", "planck_
 parser.add_argument("--mask_two", choices=["sdss_mask", "planck_point", "planck_galactic", "planck_modified_galactic", "planck_modified_point", "act"],
                     help="The second mask to use", default="act")
 parser.add_argument("--path_raise", type=int, default=2)
-parser.add_argument("--res", type=int, default=1e3)
+parser.add_argument("--nside_internal", type=int, default=1e3)
 parser.add_argument("--nside", type=int, default=256)
 parser.add_argument("--save_path")
 
@@ -18,5 +18,5 @@ mask_names = [args.mask_one, args.mask_two]
 mask1 = toolkit.load_mask(args.mask_one)
 mask2 = toolkit.load_mask(args.mask_two)
 
-toolkit.gen_mask_comparison_map(mask1, mask2, NSIDE=args.nside, res=int(1e3), name=f"{args.mask_one}_{args.mask_two}")
+toolkit.gen_mask_comparison_map(mask1, mask2, NSIDE=args.nside, NSIDE_internal=args.nside_internal, name=f"{args.mask_one}_{args.mask_two}")
 
