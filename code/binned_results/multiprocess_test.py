6a78557864f4da867c00cc8a4a1be2346d37b7f6
thomas <wardt4996@gmail.com>
2024-03-05 20:04:12 +0000

    changed stuff

--- code/binned_results/multiprocess_test.py
+++ code/binned_results/multiprocess_test.py
@@ -8,7 +8,7 @@ import threading
 #os.system("taskset -p 0xff %d" % os.getpid())
 print(multiprocessing.cpu_count())
 def test(_data):
-    os.system("taskset -p 0xff %d" % os.getpid())
+    #os.system("taskset -p 0xff %d" % os.getpid())
     data = np.random.randn(2, 1000)
     for nn in range(1000):
         for ii in data[0, :]:
