f2dd236eb7191a8f498e5ab4b6f8c341e85d1868
thomas <wardt4996@gmail.com>
2024-03-05 20:03:42 +0000

    changed stuff

--- code/binned_results/multiprocess_test.py
+++ code/binned_results/multiprocess_test.py
@@ -3,11 +3,10 @@ import multiprocessing.pool
 import os
 import time
 import psutil
+import threading
 
-os.system("taskset -p 0xff %d" % os.getpid())
-
+#os.system("taskset -p 0xff %d" % os.getpid())
 print(multiprocessing.cpu_count())
-
 def test(_data):
     os.system("taskset -p 0xff %d" % os.getpid())
     data = np.random.randn(2, 1000)
@@ -16,20 +15,21 @@ def test(_data):
             for jj in data[1, :]:
                 ii * jj
     return True
-
-
-results = []
+"""results = []
 pool = multiprocessing.pool.ThreadPool(processes=10)
 for i in range(10):
     results.append(pool.apply_async(test, (i,)))
     print(i)
-
 psutil.cpu_count()
 p = psutil.Process()
 print(p.cpu_affinity())
-
 pool.close()
 pool.join()
-
 for i in range(10):
-    results[i].get()
+    results[i].get()"""
+
+thread_1 = threading.Thread(target=test, args=(1,))
+thread_2 = threading.Thread(target=test, args=(2,))
+
+thread_1.join()
+thread_2.join()
