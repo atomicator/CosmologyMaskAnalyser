159171d603454060577096778b6215348f3e0740
thomas <wardt4996@gmail.com>
2024-03-08 14:27:43 +0000

    changed stuff

--- code/bias_test/bias_test.py
+++ code/bias_test/bias_test.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from toolkit import toolkit, weights
 import argparse
 import multiprocessing.pool
@@ -13,7 +15,7 @@ parser.add_argument("--nside", type=int, default=2)
 parser.add_argument("--weight_function", default="excess")
 parser.add_argument("--threads", type=int, default=1)
 parser.add_argument("--iterations", type=int, default=1)
-parser.add_argument("--target", type=int, default=1000)
+parser.add_argument("--target", type=int, default=400000)
 parser.add_argument("--data_mask", default="sdss_act")
 parser.add_argument("--overdensity", type=float, default=0.05)
 args = parser.parse_args()
@@ -27,9 +29,9 @@ if args.data_mask == "sdss_act":
     point_mask = toolkit.load_mask("act_point", raise_dir=args.raise_dir)
     temp1 = toolkit.load_mask("sdss_mask", raise_dir=args.raise_dir)
     temp1.map = np.int_(temp1.map)
-    temp2 = toolkit.load_mask("act_point", raise_dir=args.raise_dir)
+    #temp2 = toolkit.load_mask("act_point", raise_dir=args.raise_dir)
     temp1.map = 1 - temp1.map
-    overdensity_mask = toolkit.CombinationMask(temp1, temp2, invert=True, use_and=False)
+    overdensity_mask = toolkit.CombinationMask(temp1, point_mask, invert=True, use_and=False)
     sky_mask_frac = 0.02024328631080069
     data_set = np.float_(np.array((
         toolkit.HealpyMask("../" * args.raise_dir + f"code/binned_results/sdss_mask_act_point_256_1.fits").map,
@@ -46,12 +48,18 @@ threads = []
 
 
 def func():
+    global data_set
     random_points = toolkit.gen_random_coords(args.target, random_mask).transpose()[::-1]
     bias_points = toolkit.gen_random_coords(len(random_points) * args.overdensity * sky_mask_frac * 5, overdensity_mask).transpose()[::-1]
+    plt.scatter(random_points.transpose())
+    plt.show()
+    plt.scatter(bias_points.transpose())
+    plt.show()
     cat = toolkit.StarCatalogue()
     cat.lon_lat = np.append(random_points, bias_points[:int(len(random_points) * args.overdensity * sky_mask_frac)], axis=0)
-    return toolkit.run_nside(n=args.nside, data_set=data_set, mask=point_mask, filter_set=filter_set, a=0, cat=cat,
-                             weight_function=weights.excess_measurement, convert_to_mask_frac=False)
+    print(np.shape(cat.lon_lat))
+    return toolkit.run_nside(args.nside, data_set, point_mask, filter_set, 0, cat,
+                             weights.excess_measurement, False)
 
 
 
