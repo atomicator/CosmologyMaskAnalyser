8eda832eb994767e5af1f2fcc8020be5041f6784
thomas <wardt4996@gmail.com>
2024-03-16 04:39:45 +0000

    hopefully fixed stuff

--- batch_files/bias.sh
+++ batch_files/bias.sh
@@ -5,21 +5,33 @@ python3 ../code/binned_results/debug_script.py --save_path="rot_2.png" --lon_shi
 python3 ../code/binned_results/debug_script.py --save_path="rot_3.png" --lon_shift=3.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_4.png" --lon_shift=4.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_5.png" --lon_shift=5.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
+
+wait
+
 python3 ../code/binned_results/debug_script.py --save_path="rot_10.png" --lon_shift=10.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_15.png" --lon_shift=15.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_20.png" --lon_shift=20.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_25.png" --lon_shift=25.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_30.png" --lon_shift=30.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
+
+wait
+
 python3 ../code/binned_results/debug_script.py --save_path="rot_40.png" --lon_shift=40.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_50.png" --lon_shift=50.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_60.png" --lon_shift=60.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_70.png" --lon_shift=70.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_80.png" --lon_shift=80.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
+
+wait
+
 python3 ../code/binned_results/debug_script.py --save_path="rot_90.png" --lon_shift=90.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_100.png" --lon_shift=100.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_110.png" --lon_shift=110.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_120.png" --lon_shift=120.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_130.png" --lon_shift=130.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
+
+wait
+
 python3 ../code/binned_results/debug_script.py --save_path="rot_140.png" --lon_shift=140.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_150.png" --lon_shift=150.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
 python3 ../code/binned_results/debug_script.py --save_path="rot_160.png" --lon_shift=160.0 --data_mask="sdss_act_lon_shift" --raise_path=1 &
