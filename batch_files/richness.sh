350643baba5a3438cb8dae3a8b1d0d33144cecaf
thomas <wardt4996@gmail.com>
2024-04-09 22:30:12 +0100

    hopefully fixed stuff

--- batch_files/richness.sh
+++ batch_files/richness.sh
@@ -17,6 +17,8 @@ python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="s
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_s_8.png" --min_r=71.41545253 --mask_set="both" --data_mask="sdss_planck" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered"  --weight_function="excess" --save_path="./act_s_8.png" --min_r=71.41545253 --mask_set="both" --data_mask="sdss_act" &
 
+wait
+
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_s_13.png" --min_r=21.3796209 --max_r=40.4324751 --mask_set="both" --data_mask="sdss_planck" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered"  --weight_function="excess" --save_path="./act_s_13.png" --min_r=21.3796209 --max_r=40.4324751 --mask_set="both" --data_mask="sdss_act" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_s_35.png" --min_r=40.4324751 --max_r=54.37541725 --mask_set="both" --data_mask="sdss_planck" &
