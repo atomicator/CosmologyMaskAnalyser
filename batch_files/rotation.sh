03a46eea9dda6ce98ed30f5e306479240dc982fc
thomas <wardt4996@gmail.com>
2024-04-14 21:59:48 +0100

    hopefully fixed stuff

--- batch_files/rotation.sh
+++ batch_files/rotation.sh
@@ -15,20 +15,20 @@ python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-30
 
 wait
 
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-40.png" --lon_shift=40.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-50.png" --lon_shift=50.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-60.png" --lon_shift=60.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-70.png" --lon_shift=70.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-80.png" --lon_shift=80.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-90.png" --lon_shift=90.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-100.png" --lon_shift=100.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-110.png" --lon_shift=110.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-120.png" --lon_shift=120.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-130.png" --lon_shift=130.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-140.png" --lon_shift=140.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-150.png" --lon_shift=150.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-160.png" --lon_shift=160.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-170.png" --lon_shift=170.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
-python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-180.png" --lon_shift=180.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
--
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-40.png" --lon_shift=-40.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-50.png" --lon_shift=-50.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-60.png" --lon_shift=-60.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-70.png" --lon_shift=-70.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-80.png" --lon_shift=-80.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-90.png" --lon_shift=-90.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-100.png" --lon_shift=-100.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-110.png" --lon_shift=-110.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-120.png" --lon_shift=-120.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-130.png" --lon_shift=-130.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-140.png" --lon_shift=-140.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-150.png" --lon_shift=-150.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-160.png" --lon_shift=-160.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-170.png" --lon_shift=-170.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+python3 ../code/binned_results/debug_script.py --save_path="${mask_name}_rot_-180.png" --lon_shift=-180.0 --data_mask="${mask_name}" --raise_path=1 --catalogue="sdss_filtered" --min_r=20 &
+
 wait
