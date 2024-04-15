0fee59b37dd1739dd3d2d830b2f906e4987011f5
thomas <wardt4996@gmail.com>
2024-04-15 15:45:47 +0100

    hopefully fixed stuff

--- code/plot_scripts/act_mask.py
+++ code/plot_scripts/act_mask.py
@@ -62,7 +62,7 @@ plot_mask.plot(cbar=False, label="1", cmap="bwr", show=False, clear=False)
 point_mask = toolkit.load_mask("act_point", lon_shift=25.0)
 plot_mask = point_mask
 plot_mask.set_fig_ax(fig, ax)
-plot_mask.plot(cbar=False, label="2", cmap="bwr_r", show=False, clear=False)
+plot_mask.plot(cbar=True, label="2", cmap="bwr_r", show=False, clear=False)
 #print(plot_mask.lon_shift)
 
 plt.ylabel("Latitude")
