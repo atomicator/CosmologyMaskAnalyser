fbef77fd451b1c8ea62b6903285343279e77c1f3
thomas <wardt4996@gmail.com>
2024-04-17 17:53:22 +0100

    hopefully fixed stuff

--- batch_files/richness.sh
+++ batch_files/richness.sh
@@ -53,6 +53,8 @@ python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="s
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_40_80.png" --min_r=40 --max_r=80 --mask_set="point" --data_mask="sdss_act" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_80+.png" --min_r=80 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
 
+wait
+
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_5_10.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_10_20.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_20_40.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
@@ -65,3 +67,4 @@ python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="s
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_40_80.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_80+.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
 
+wait
--- code/plot_cached_results/rotation.py
+++ code/plot_cached_results/rotation.py
@@ -23,9 +23,9 @@ toolkit.plt_use_tex()
 fig = plt.figure()
 ax = fig.add_subplot()
 
-folders = ["./rotations/act/", "./rotations/planck/"][::-1]
+folders = ["./rotations/act/", "./rotations/planck/"][::1][0:1]
 #folders = ["./rotations/planck/"]
-labels = ["ACT", "Planck"][::-1]
+labels = ["Point", "Galactic"]
 
 NSIDES = np.array((1, 2, 4, 8, 16, 32, 64, 128))
 x = np.append(np.array(1/2), NSIDES)
@@ -33,12 +33,17 @@ x = np.append(np.array(1/2), NSIDES)
 y = np.zeros((len(folders), len(x)))
 y_std = np.zeros((len(folders), len(x)))
 
+use_abs = False
+
 for folder in folders:
     files = os.listdir(folder)
     sigma = np.zeros((len(files), len(x)))
     for file in files:
         data = np.load(folder + file)[0]
-        sigma[files.index(file)] = np.abs(data[0] / data[1])
+        if use_abs:
+            sigma[files.index(file)] = np.abs(data[0] / data[1])
+        else:
+            sigma[files.index(file)] = data[0] / data[1]
     for i in range(len(y[0])):
         y[folders.index(folder)][i] = np.mean(sigma[:, i])
         y_std[folders.index(folder)][i] = np.std(sigma[:, i])
@@ -49,12 +54,18 @@ for i in range(len(folders)):
     plt.errorbar(x, y[i], y_std[i], label=labels[i], ecolor=error_color[i], color=color[i], capsize=3,
                  capthick=1,)
 
-plt.plot((1/2, np.max(NSIDES)), (0.8, 0.8), color="k", linestyle="dashed")
-plt.plot((1/2, np.max(NSIDES)), (0.2, 0.2), color="k", linestyle="dotted")
-plt.plot((1/2, np.max(NSIDES)), (1.4, 1.4), color="k", linestyle="dotted")
+if use_abs:
+    plt.plot((1/2, np.max(NSIDES)), (0.8, 0.8), color="k", linestyle="dashed")
+    plt.plot((1/2, np.max(NSIDES)), (0.2, 0.2), color="k", linestyle="dotted")
+    plt.plot((1/2, np.max(NSIDES)), (1.4, 1.4), color="k", linestyle="dotted")
+else:
+    plt.plot((1 / 2, np.max(NSIDES)), (1, 1), color="k", linestyle="dashed")
+    plt.plot((1 / 2, np.max(NSIDES)), (0, 0), color="k", linestyle="dotted")
+    plt.plot((1 / 2, np.max(NSIDES)), (-1, -1), color="k", linestyle="dotted")
 
+plt.title("Normalised mean detection for the rotated ACT mask")
 plt.xscale("log")
 plt.legend()
 ax.set_xticks(x, ["C"] + list(NSIDES))
-plt.savefig("test.pdf")
+plt.savefig("act_rot.pdf")
 plt.show()
