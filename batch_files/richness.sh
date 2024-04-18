ffb888895d6be89fe4b3f888474a494a856aab92
thomas <wardt4996@gmail.com>
2024-04-18 20:13:25 +0100

    hopefully fixed stuff

--- batch_files/richness.sh
+++ batch_files/richness.sh
@@ -47,8 +47,6 @@ python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="s
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_40_80.png" --min_r=40 --max_r=80 --mask_set="point" --data_mask="sdss_planck" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_80+.png" --min_r=80 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
 
-wait
-
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_5_10.png" --min_r=5 --max_r=10 --mask_set="point" --data_mask="sdss_act" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_10_20.png" --min_r=10 --max_r=20 --mask_set="point" --data_mask="sdss_act" &
 python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_20_40.png" --min_r=20 --max_r=40 --mask_set="point" --data_mask="sdss_act" &
@@ -57,18 +55,12 @@ python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="s
 
 wait
 
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_5_10.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_10_20.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_20_40.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_40_80.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_80+.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" &
+python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_z_0_3.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" --min_z=0.0 --max_z=0.3 &
+python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_z_3_45.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" --min_z=0.3 --max_z=0.45 &
+python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./planck_r_45_6.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_planck" --min_z=0.45 --max_z=0.6 &
 
-wait
-
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_5_10.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_10_20.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_20_40.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_40_80.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
-python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_80+.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" &
+python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_z_0_3.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" --min_z=0.0 --max_z=0.3 &
+python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_z_3_45.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" --min_z=0.3 --max_z=0.45 &
+python3 "./../code/binned_results/debug_script.py" --raise_path=1 --catalogue="sdss_filtered" --weight_function="excess" --save_path="./act_r_45_6.png" --min_r=20 --max_r=8000 --mask_set="point" --data_mask="sdss_act" --min_z=0.45 --max_z=0.6 &
 
 wait
