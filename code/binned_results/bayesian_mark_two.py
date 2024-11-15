823b16e96cf87a1db999e999bf9061b6b948a40c
tom <wardt4996@gmail.com>
2024-11-15 00:01:03 +0000

    debug

--- code/binned_results/bayesian_mark_two.py
+++ code/binned_results/bayesian_mark_two.py
@@ -85,6 +85,7 @@ def to_thread():
         bias_points = bias_points[sdss_mask.lookup_point(*bias_points.transpose()) != 0]
     cat.lon_lat = np.append(arr=random_points, values=bias_points, axis=0)
     print(len(cat.lon_lat), len(random_points), len(bias_points))
+    exit()
     for NSIDE in NSIDES:
         results = np.zeros(overdensity_steps)  # replace with mutex
         print("Resizing sky fractions")
