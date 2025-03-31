53ca33fa0ab166710401d55654d76d3bde49bf72
tom <wardt4996@gmail.com>
2025-04-01 00:25:02 +0100

    Debugging memory usage

--- toolkit/toolkit.py
+++ toolkit/toolkit.py
@@ -528,7 +528,7 @@ def gen_mask_comparison_map(mask1, mask2, NSIDE=512, NSIDE_internal=2048, name="
     data = np.float32(np.zeros(pix.size))
 
     steps = 1000
-    divisions = np.int_(np.linspace(0, pix.shape, steps + 1))
+    divisions = np.int_(np.linspace(0, pix.shape[0], steps + 1))
 
     print("Querying masks")
     for i in range(steps):
