18915b48197f4012cc1b31b8408d59477a59967e
tom <wardt4996@gmail.com>
2025-04-18 08:12:54 +0100

    Add skip flag to generate_mask_map.py

--- code/binned_results/generate_mask_map.py
+++ code/binned_results/generate_mask_map.py
@@ -10,6 +10,7 @@ parser.add_argument("--path_raise", type=int, default=2)
 parser.add_argument("--nside_internal", type=int, default=65536)
 parser.add_argument("--nside", type=int, default=512)
 parser.add_argument("--threads", type=int, default=1)
+parser.add_argument("--skip", type=int, default=0)
 parser.add_argument("--save_path")
 
 args = parser.parse_args()
@@ -23,4 +24,4 @@ kwargs = {"raise_dir":args.path_raise}
 toolkit.gen_mask_comparison_map(data.load_mask, args=[], kwargs=kwargs, mask1_name=args.mask_one,
                                 mask2_name=args.mask_two, NSIDE=args.nside, NSIDE_internal=args.nside_internal,
                                 name="../" * args.path_raise + f"./data/{args.mask_one}_{args.mask_two}",
-                                num_thread=args.threads)
+                                num_thread=args.threads, skip=args.skip)
